1f84030b56288dfcd507d001d1c3bfd961fa79c8
VishalBodkhe <vishbodkhe@gmail.com>
2019-09-07 18:18:04 +0530

    Added TODO onlick method

--- app/src/main/java/extra/train/example/com/gitfirstapp/MainActivity.java
+++ app/src/main/java/extra/train/example/com/gitfirstapp/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Onlick method Nick
+                //Onlick method to click
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
